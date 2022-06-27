f214d659f403acb2b67e3190267ca662a2e9c30e
kevin.xu <kevin.xu@imaginato.com>
2022-06-27 15:01:55 +0800

    second commit

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.kevin.javafxdemo2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires kotlin.stdlib;
-
-
-    opens com.kevin.javafxdemo2 to javafx.fxml;
-    exports com.kevin.javafxdemo2;
-}
